2cffe68ff00db654b85090381eb2c940ff3f5409
Matthew Malone <m.malone@me.com>
2019-12-03 14:06:42 -0700

    Update the knexfile development and production migrations

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/picasso-db',
+    migrations: {
+      directory: './db/migrations'
+    },
     useNullAsDefault: true
   },
 
@@ -43,6 +46,12 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/picasso-db',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true,
   },
 
