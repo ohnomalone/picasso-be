fcc3ddbfa2e1f86d3988f2e1b3d26d9a4d36a1b4
Matthew Malone <m.malone@me.com>
2019-12-03 14:19:43 -0700

    Create tables in migrations

--- db/migrations/20191203140706_initial.js
+++ db/migrations/20191203140706_initial.js
@@ -1,6 +1,33 @@
 
 exports.up = function(knex) {
-  
+    return Promise.all([
+        knex.schema.createTable('catalogss', function(table) {
+          table.increments('id').primary();
+          table.string('firstName');
+          table.string('lastName');
+          table.string('email');
+          table.string('password');
+          table.timestamps(true, true);
+        }),
+    
+        knex.schema.createTable('catalogs', function(table) {
+          table.increments('id').primary();
+          table.string('catalogName');
+          table.integer('user_id').unsigned()
+          table.foreign('user_id')
+            .references('users.id');
+          table.timestamps(true, true);
+        }),
+
+        knex.schema.createTable('palettes', function(table) {
+            table.increments('id').primary();
+            table.string('palletName');
+            table.integer('catalog_id').unsigned()
+            table.foreign('catalog_id')
+              .references('catalogs.id');
+            table.timestamps(true, true);
+          })
+      ])
 };
 
 exports.down = function(knex) {
