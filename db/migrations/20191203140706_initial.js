5c8af664cc9981ce7fc9105394c7a259a852d513
Matthew Malone <m.malone@me.com>
2019-12-04 13:52:12 -0700

    Update migration schema email and color formats
    Co-authored by: <edwindelbosquer@gmail.com>

--- db/migrations/20191203140706_initial.js
+++ db/migrations/20191203140706_initial.js
@@ -1,11 +1,10 @@
-
 exports.up = function(knex) {
     return Promise.all([
         knex.schema.createTable('users', function(table) {
           table.increments('id').primary();
           table.string('firstName');
           table.string('lastName');
-          table.string('email');
+          table.unique('email');
           table.string('password');
           table.timestamps(true, true);
         }),
@@ -22,11 +21,11 @@ exports.up = function(knex) {
         knex.schema.createTable('palettes', function(table) {
             table.increments('id').primary();
             table.string('palletName');
-            table.string('color1');
-            table.string('color2');
-            table.string('color3');
-            table.string('color4');
-            table.string('color5');
+            table.json('color1');
+            table.json('color2');
+            table.json('color3');
+            table.json('color4');
+            table.json('color5');
             table.integer('catalog_id').unsigned()
             table.foreign('catalog_id')
               .references('catalogs.id');
