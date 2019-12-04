8c9a8e9be0d7864d92f8887eb1ba32adfff369fa
Matthew Malone <m.malone@me.com>
2019-12-04 16:11:39 -0700

    Create app.js and app.test.js files, setup testing boilerplate in each file

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+app.locals.title = 'Test Express';
+app.use(cors());
+app.use(express.json());
+
+// All endpoints live here
+
+app.get('/', (request, response) => {
+  response.send('We\'re going to test all the routes!');
+});
