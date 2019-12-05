8ca1b7c49d99d229f3a530f380ffd1cf3da66718
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-04 17:15:05 -0700

    Install supertest and other babel dependencies for testing

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.locals.title = 'Test Express';
 app.use(cors());
@@ -12,7 +12,7 @@ app.use(express.json());
 // All endpoints live here
 
 app.get('/', (request, response) => {
-  response.send('We\'re going to test all the routes!');
+	response.send("We're going to test all the routes!");
 });
 
 export default app;
